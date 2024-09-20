06731178b4cd0ffc5b71905c8c943c8f12a48113
CristianoRico <carvalhocristianorico@gmail.com>
2024-09-20 11:13:03 +0700

    update index.js

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const server = http.createServer( (req, res) => {
     // res.write('Selamat Pagi');
 
     res.writeHead(200, { 'Content-Type': 'application/json'});
+
+    if(req.url === '/dosen'){
+        message = 'List Data Dosen';
+        data = ['Ahmad', 'Hafiz', 'Widi'];
+    } else if(req.url === '/mahasiswa') {
+        message = 'List Data Mahasiswa';
+        data = ['Kenny', 'Rizky', 'Akbar'];
+    } else {
+        message = 'Tidak Ditemukan';
+        data = [];
+    }
     res.write(JSON.stringify({
-        'message' : 'selamat pagi',
+        'message' : message,
+        'data' : data,
         'status' : 'success'
     }));
     res.end();
